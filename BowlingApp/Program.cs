8a7d467718fde7f85da003f7d7ebaf7da93c9648
Felix Cau <130475694+Felix-Cau@users.noreply.github.com>
2025-02-25 21:49:43 +0100

    DbContext fixed and DI for DbContext + UserRepo fixed

--- BowlingApp/Program.cs
+++ BowlingApp/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BowlingApp.Repository;
+using BowlingApp.Repository.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+//Getting Configuration to check for appsettings.json
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+    
+//Getting the connectionstring
+string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+//Setting upp DI
+var services = new ServiceCollection()
+.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString))
+.AddScoped<UserRepository>()
+.BuildServiceProvider();
