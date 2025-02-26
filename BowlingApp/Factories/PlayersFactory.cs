769c7928f38d30968d8d028a62ba9be68bca46fc
Felix Cau <130475694+Felix-Cau@users.noreply.github.com>
2025-02-26 09:24:01 +0100

    PlayersFactory v.1

---
+++ BowlingApp/Factories/PlayersFactory.cs
@@ -0,0 +1,14 @@
+﻿using BowlingApp.Interfaces;
+
+namespace BowlingApp.Factories;
+
+public class PlayersFactory
+{
+    public List<IPlayer> Participants(IPlayer playerOne, IPlayer playerTwo)
+    {
+        var players = new List<IPlayer>();
+        players.Add(playerOne);
+        players.Add(playerTwo);
+        return players;
+    }
+}
